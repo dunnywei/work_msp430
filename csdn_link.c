d4d1b28b0694faf747c0e1cf0cca79a3a5454b8d
dunnywei <wei_don@yahoo.com>
2019-05-29 00:02:47 -0400

    2

--- csdn_link.c
+++ csdn_link.c
@@ -1,4 +1,5 @@
 1->MSP430学习笔记1——msp430概述
 https://blog.csdn.net/mao_hui_fei/article/details/80482465
 
-2->
+2->MSP430单片机各种寄存器总结（5）——Timer_A0
+https://blog.csdn.net/weixin_43830248/article/details/85217404
