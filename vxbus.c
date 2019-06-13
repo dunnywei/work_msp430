2f291844c797c776b9e5dfd9b58f53b171a78e65
dunnywei <wei_don@yahoo.com>
2019-06-13 11:46:33 -0400

    1

--- vxbus.c
+++ vxbus.c
@@ -1,2 +1,5 @@
-->vxbusShow 10 and csdn
+1->vxbusShow 10 and csdn
 https://blog.csdn.net/RubyBoss/article/details/42886299
+
+2->vxbusshow i2c
+https://blog.csdn.net/qingfengtsing/article/details/19174803
