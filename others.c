4e1b5e40348d7598f1528584c0de7c01439ee6f0
dunnywei <wei_don@yahoo.com>
2019-09-25 13:19:27 -0400

    3

--- others.c
+++ others.c
@@ -2,3 +2,5 @@
 https://www.chiefdelphi.com/t/vxworks-exception-handling-and-division-by-zero/131145/2
 2->cluster memory
 https://en.wikipedia.org/wiki/Distributed_memory
+3->check memory and malloc in vxWorks shell
+https://stackoverflow.com/questions/23701967/how-to-define-an-array-in-vxworks-shell
