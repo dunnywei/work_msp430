e151044720c67c0cf580c400206e005da281bd5e
dunnywei <wei_don@yahoo.com>
2019-09-25 12:54:28 -0400

    cluster memory

--- others.c
+++ others.c
@@ -1,3 +1,4 @@
 1->divide by 0 and vxworks
 https://www.chiefdelphi.com/t/vxworks-exception-handling-and-division-by-zero/131145/2
-2->
+2->cluster memory
+https://en.wikipedia.org/wiki/Distributed_memory
