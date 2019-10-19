7bd3ef87eee3cc802ed0a785605cca2e98692436
dunnywei <wei_don@yahoo.com>
2019-10-18 23:33:39 -0400

    extern in C

--- others.c
+++ others.c
@@ -4,3 +4,5 @@ https://www.chiefdelphi.com/t/vxworks-exception-handling-and-division-by-zero/13
 https://en.wikipedia.org/wiki/Distributed_memory
 3->check memory and malloc in vxWorks shell
 https://stackoverflow.com/questions/23701967/how-to-define-an-array-in-vxworks-shell
+4->extern in C
+https://www.geeksforgeeks.org/understanding-extern-keyword-in-c/
