0d1583380efb2f72d54f7ef9868c0e7dd34286e3
dunnywei <wei_don@yahoo.com>
2019-10-19 17:29:18 -0400

    5

--- others.c
+++ others.c
@@ -6,3 +6,5 @@ https://en.wikipedia.org/wiki/Distributed_memory
 https://stackoverflow.com/questions/23701967/how-to-define-an-array-in-vxworks-shell
 4->extern in C
 https://www.geeksforgeeks.org/understanding-extern-keyword-in-c/
+5->deoxygen in centos
+https://gist.github.com/slog2/cc853c34ded1dc1622165e95d95acd1c
