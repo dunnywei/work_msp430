9dcf1d33e7a01ae95dbc2b4b6c9d14a2015f88b4
dunnywei <wei_don@yahoo.com>
2019-10-31 11:52:55 -0400

    3 for qos

--- network_ip.c
+++ network_ip.c
@@ -3,3 +3,5 @@ https://netref.soe.ucsc.edu/node/31
 2->ftp command
 https://www.computerhope.com/issues/ch001246.htm
 https://docs.oracle.com/cd/E19120-01/open.solaris/819-1634/remotehowtoaccess-40629/index.html
+3->qos
+https://www.youtube.com/watch?v=dSEEwHCvOnA
