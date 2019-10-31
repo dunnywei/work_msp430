fe523d0455d8581b3bcaac535f6153f971884880
dunnywei <wei_don@yahoo.com>
2019-10-31 11:11:35 -0400

    2

--- network_ip.c
+++ network_ip.c
@@ -1,2 +1,5 @@
 1->ttcp
 https://netref.soe.ucsc.edu/node/31
+2->ftp command
+https://www.computerhope.com/issues/ch001246.htm
+https://docs.oracle.com/cd/E19120-01/open.solaris/819-1634/remotehowtoaccess-40629/index.html
