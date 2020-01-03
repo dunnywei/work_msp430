31579b00035ab3c64deab2869efe4df4207dd88c
dunnywei <wei_don@yahoo.com>
2020-01-03 14:58:51 -0500

    bulk in transfer

--- usb.c
+++ usb.c
@@ -1,3 +1,5 @@
 1->usb frame
 https://microchipdeveloper.com/usb:frames
 http://www.usbmadesimple.co.uk/ums_6.htm
+2->bulk transfer in usb
+https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/usb-bulk-and-interrupt-transfer
